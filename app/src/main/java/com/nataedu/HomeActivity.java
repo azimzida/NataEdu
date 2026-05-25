255f385d7d4eedb3292a536bdf0d7d7ee9da3516
fawwaz1024 <fawwazsulthon2005@gmail.com>
2026-05-25 13:52:54 +0700

    Update

--- app/src/main/java/com/nataedu/HomeActivity.java
+++ app/src/main/java/com/nataedu/HomeActivity.java
@@ -1,21 +1,28 @@
 package com.nataedu;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.firestore.FirebaseFirestore;
-import java.util.HashMap;
-import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page); // Memanggil layout home_page.xml
+        setContentView(R.layout.home_page);
+
+        LinearLayout navHome = findViewById(R.id.navHome);
+        LinearLayout navCourse = findViewById(R.id.navCourse);
+        LinearLayout navMentor = findViewById(R.id.navMentor);
+
+        navHome.setOnClickListener(v -> {
+            // udah di home
+        });
 
 
 
+        navMentor.setOnClickListener(v -> {
+            startActivity(new Intent(this, MentorActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/com/nataedu/MentorActivity.java
@@ -0,0 +1,12 @@
+package com.nataedu;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MentorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mentor);
+    }
+}
